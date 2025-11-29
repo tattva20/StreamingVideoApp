f6425d946841c79857467e87fdda9712a0f4215d
tattva20 <financieraufc@gmail.com>
2025-11-29 10:00:52 -0600

    Test init does not message store upon creation (validate cache use case) Started implementing validate video cache use case tests by adding the first test to verify that LocalVideoLoader initialization does not send any messages to the store. Following the , cache validation is handled by the same LocalVideoLoader class but through a different method (validateCache). This test establishes the baseline behavior for the validation functionality.

---
+++ StreamingCoreTests/Video Cache/ValidateVideoCacheUseCaseTests.swift
@@ -0,0 +1,24 @@
+import XCTest
+import StreamingCore
+
+@MainActor
+class ValidateVideoCacheUseCaseTests: XCTestCase {
+
+    func test_init_doesNotMessageStoreUponCreation() {
+        let (_, store) = makeSUT()
+
+        XCTAssertEqual(store.receivedMessages, [])
+    }
+
+    // MARK: - Helpers
+
+    private func makeSUT(currentDate: @escaping () -> Date = Date.init,
+                         file: StaticString = #filePath,
+                         line: UInt = #line) -> (sut: LocalVideoLoader, store: VideoStoreSpy) {
+        let store = VideoStoreSpy()
+        let sut = LocalVideoLoader(store: store, currentDate: currentDate)
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, store)
+    }
+}
